0bbdde92f485c2e129989823c9e5cb238063fb25
Lucarus <lukas_sturm@yahoo.de>
2016-05-21 20:06:31 +0200

    Fenster mit Layout hinzugefügt

---
+++ src/Kontaktbuch/Fenster/MainWindow.java
@@ -0,0 +1,12 @@
+package Kontaktbuch.Fenster;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel panel1;
+    private JTabbedPane tabPaneKontakte;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JTextArea textArea4;
+}
