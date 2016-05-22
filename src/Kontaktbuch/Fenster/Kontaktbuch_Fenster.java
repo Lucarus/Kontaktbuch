7e3f74bb5150ad25da72512ce046f4729f67c1b3
Lucarus <lukas_sturm@yahoo.de>
2016-05-22 23:02:20 +0200

    Fügt übergebene Kontakte zur TabControl hinzu

--- src/Kontaktbuch/Fenster/Kontaktbuch_Fenster.java
+++ src/Kontaktbuch/Fenster/Kontaktbuch_Fenster.java
@@ -2,38 +2,29 @@ package Kontaktbuch.Fenster;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
+import Kontaktbuch.Kontakt_Klassen.*;
 
 public class Kontaktbuch_Fenster extends JFrame{
 
     JPanel hauptfenster = new JPanel();
     JTabbedPane tabControl = new JTabbedPane();
-    JPanel startSeite = new JPanel();
-    JPanel testSeite = new JPanel();
 
-    public Kontaktbuch_Fenster() {
+    public Kontaktbuch_Fenster(ArrayList<Kontakt> kontakte) {
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setMinimumSize( new Dimension(300, 300));
 
-        startSeite.add(new JLabel("Das ist ein Test"));
-        testSeite.add(new JButton("Hey"));
-
-
-        for (int i = 0; i < 5; i++) {
-            testSeite = new JPanel();
-            testSeite.add(new JLabel("töööscht" + i));
-            tabControl.add("Neee" + i, testSeite);
+        for (Kontakt kontakt:kontakte) {
+            tabControl.add(kontakt.getVorname(), new KontaktPanel(kontakt));
         }
 
-
         hauptfenster.setLayout(new BorderLayout());
-
         hauptfenster.add(tabControl, BorderLayout.CENTER);
 
         getContentPane().add(hauptfenster);
 
         pack();
-
         setVisible(true);
 
     }
